533a46e63e0b0d8956ce0131a4d2f37f634e2d35
Zhian N. Kamvar <zkamvar@gmail.com>
2020-01-24 11:14:47 +0000

    update find breaks for error handling

--- R/find_breaks.R
+++ R/find_breaks.R
@@ -22,6 +22,11 @@
 #' # Include the value itself
 #' find_breaks(123, snap = 25, ceiling = TRUE)
 find_breaks <- function(n, breaks = 4, snap = 1, ceiling = FALSE) {
+  chk <- function(i) length(i) != 1 || !is.finite(i)
+  pos <- function(i) i <= 0
+  if (chk(n)) stop("n must be a single finite number")
+  if (chk(breaks) || pos(breaks)) stop("breaks must be a single, positive finite number")
+  if (chk(snap) || pos(snap)) stop("snap must be a single, positive finite number")
   if (snap >= n) stop(sprintf("snap (%d) must be smaller than n (%d)", snap, n))
   seq_by <- ceiling((n/breaks)/snap) * snap
   res <- seq(1, n, by = seq_by)
